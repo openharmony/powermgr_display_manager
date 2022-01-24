7c4ba9659ec5cdb768c435f0bcfede517f897b81
ShiJie <shijie20@huawei.com>
2022-01-24 15:33:02 +0800

    fix: need to call set brightness
    Signed-off-by: ShiJie <shijie20@huawei.com>
    Change-Id: I5ce0e502b84f681caea7f6a174c6773eecd6a85b

--- service/native/include/screen_controller.h
+++ service/native/include/screen_controller.h
@@ -47,6 +47,7 @@ public:
 private:
     static const uint32_t SCREEN_BRIGHTNESS_UPDATE_DURATION = 200;
     void OnStateChanged(DisplayState state);
+    void ScreenOnReset(DisplayState state);
     std::mutex mutex_;
     const uint64_t devId_;
     DisplayState state_;
