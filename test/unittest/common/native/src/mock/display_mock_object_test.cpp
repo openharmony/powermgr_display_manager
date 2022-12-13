c588286e9f9957ceb0199af4281d95ea3157cbab
zblue <zhengpengyue1@huawei.com>
2022-12-13 20:28:09 +0800

    test: fix failed testcase
    Signed-off-by: zblue <zhengpengyue1@huawei.com>
    Change-Id: I5c425093d9462fb173e2353ae36f0bd982436cf8

--- test/unittest/common/native/src/mock/display_mock_object_test.cpp
+++ test/unittest/common/native/src/mock/display_mock_object_test.cpp
@@ -107,7 +107,6 @@ HWTEST_F(DisplayMockObjectTest, DisplayMockObjectTest_002, TestSize.Level0)
     EXPECT_EQ(BRIGHTNESS_DEFAULT_VALUE, client.GetDefaultBrightness());
     EXPECT_EQ(BRIGHTNESS_MAX_VALUE, client.GetMaxBrightness());
     EXPECT_EQ(BRIGHTNESS_MIN_VALUE, client.GetMinBrightness());
-    EXPECT_FALSE(client.AutoAdjustBrightness(true));
     EXPECT_TRUE(client.AdjustBrightness(BRIGHTNESS_ADJUST_VALUE, BRIGHTNESS_DURATION, DISPLAY_ID));
     EXPECT_FALSE(client.AutoAdjustBrightness(false));
     EXPECT_FALSE(client.IsAutoAdjustBrightness());
