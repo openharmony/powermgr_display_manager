3527155e1c55117a63a60de6a9fae302263c4fd5
volcano6322 <wangcan61@huawei.com>
2023-08-07 02:37:23 +0000

    fix: To solve the problem of turning off the screen when init
    Signed-off-by: volcano6322 <wangcan61@huawei.com>

--- service/native/src/display_system_ability.cpp
+++ service/native/src/display_system_ability.cpp
@@ -27,14 +27,19 @@ REGISTER_SYSTEM_ABILITY_BY_ID(DisplaySystemAbility, DISPLAY_MANAGER_SERVICE_ID, 
 }
 void DisplaySystemAbility::OnStart()
 {
-    DISPLAY_HILOGE(COMP_SVC, "Add DISPLAY_MANAGER_SERVICE_SA_ID Listener.");
+    DISPLAY_HILOGI(COMP_SVC, "DisplayPowerService On Start.");
     AddSystemAbilityListener(DISPLAY_MANAGER_SERVICE_SA_ID);
+}
 
-    DISPLAY_HILOGI(COMP_SVC, "Start service");
-    auto service = DelayedSpSingleton<DisplayPowerMgrService>::GetInstance();
-    service->Init();
-    if (!Publish(service)) {
-        DISPLAY_HILOGE(COMP_SVC, "Failed to publish service");
+void DisplaySystemAbility::OnAddSystemAbility(int32_t systemAbilityId, const std::string& deviceId)
+{
+    if (systemAbilityId == DISPLAY_MANAGER_SERVICE_SA_ID) {
+        DISPLAY_HILOGI(COMP_SVC, "Start service");
+        auto service = DelayedSpSingleton<DisplayPowerMgrService>::GetInstance();
+        service->Init();
+        if (!Publish(service)) {
+            DISPLAY_HILOGE(COMP_SVC, "Failed to publish service");
+        }
     }
 }
 
@@ -43,6 +48,7 @@ void DisplaySystemAbility::OnStop()
     DISPLAY_HILOGW(COMP_SVC, "Stop service");
     auto service = DelayedSpSingleton<DisplayPowerMgrService>::GetInstance();
     service->Deinit();
+    RemoveSystemAbilityListener(DISPLAY_MANAGER_SERVICE_SA_ID);
 }
 } // OHOS
 } // DisplayPowerMgr
