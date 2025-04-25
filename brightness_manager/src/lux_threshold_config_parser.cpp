9806872ad5e6eb1e50a65cbbaa0758cf73934e6a
gaorui <gaorui23@huawei.com>
2025-04-25 18:06:50 +0800

    fix: jsoncpp is replaced with cJSON
    Signed-off-by: gaorui <gaorui23@huawei.com>
    Change-Id: Iff3e34af32e758049c3d0d8a362bd042868bf90e

--- brightness_manager/src/lux_threshold_config_parser.cpp
+++ brightness_manager/src/lux_threshold_config_parser.cpp
@@ -15,7 +15,6 @@
 
 #include "lux_threshold_config_parser.h"
 
-#include <json/json.h>
 #include <unistd.h>
 
 #include "config_parser_base.h"
@@ -67,43 +66,76 @@ void SetDefault(LuxThresholdConfig::Data& data)
 
 using namespace OHOS::DisplayPowerMgr;
 
+void LuxThresholdConfigParser::LuxThresholdParseConfigParams(cJSON* item, LuxThresholdConfig::Data& data)
+{
+    LuxThresholdConfig::Mode config{};
+    std::string name;
+
+    const cJSON* modeNameNode = cJSON_GetObjectItemCaseSensitive(item, "modeName");
+    if (modeNameNode && cJSON_IsString(modeNameNode) && modeNameNode->valuestring &&
+        strlen(modeNameNode->valuestring) > 0) {
+        name = modeNameNode->valuestring;
+    } else {
+        DISPLAY_HILOGW(FEAT_BRIGHTNESS, "<%{public}s> modeName is not found!", CONFIG_NAME.c_str());
+        return;
+    }
+
+    const cJSON* brightenDebounceTimeNode = cJSON_GetObjectItemCaseSensitive(item, "brightenDebounceTime");
+    if (brightenDebounceTimeNode && cJSON_IsNumber(brightenDebounceTimeNode)) {
+        config.brightenDebounceTime = brightenDebounceTimeNode->valueint;
+    }
+
+    const cJSON* darkenDebounceTimeNode = cJSON_GetObjectItemCaseSensitive(item, "darkenDebounceTime");
+    if (darkenDebounceTimeNode && cJSON_IsNumber(darkenDebounceTimeNode)) {
+        config.darkenDebounceTime = darkenDebounceTimeNode->valueint;
+    }
+
+    ConfigParserBase::Get().ParsePointXy(item, "brightenPoints", config.brightenPoints);
+    ConfigParserBase::Get().ParsePointXy(item, "darkenPoints", config.darkenPoints);
+
+    data.modeArray.insert(std::make_pair(name, config));
+    DISPLAY_HILOGI(FEAT_BRIGHTNESS, "<%{public}s> is insert!", name.c_str());
+}
+
 bool LuxThresholdConfigParser::ParseConfig(int displayId, LuxThresholdConfig::Data& data)
 {
     DISPLAY_HILOGI(FEAT_BRIGHTNESS, "[%{public}d] parse LuxThresholdConfigParser start!", displayId);
-    const Json::Value root = ConfigParserBase::Get().LoadConfigRoot(displayId, CONFIG_NAME);
-    if (root.isNull()) {
+    const cJSON* root = ConfigParserBase::Get().LoadConfigRoot(displayId, CONFIG_NAME);
+    if (!root) {
         SetDefault(data);
         return false;
     }
-    if (root["isLevelEnable"].isBool()) {
-        data.isLevelEnable = root["isLevelEnable"].asBool();
+    if (!cJSON_IsObject(root)) {
+        DISPLAY_HILOGI(FEAT_BRIGHTNESS, "root is not an object!");
+        cJSON_Delete(const_cast<cJSON*>(root));
+        return false;
     }
+    const cJSON* isLevelEnableNode = cJSON_GetObjectItemCaseSensitive(root, "isLevelEnable");
+    if (isLevelEnableNode && cJSON_IsBool(isLevelEnableNode)) {
+        data.isLevelEnable = cJSON_IsTrue(isLevelEnableNode);
+    }
+
     ConfigParserBase::Get().ParsePointXy(
         root, "brightenPointsForLevel", data.brightenPointsForLevel);
     ConfigParserBase::Get().ParsePointXy(
         root, "darkenPointsForLevel", data.darkenPointsForLevel);
-    if (!root["thresholdMode"].isArray()) {
+
+    const cJSON* thresholdModeArray = cJSON_GetObjectItemCaseSensitive(root, "thresholdMode");
+    if (!thresholdModeArray || !cJSON_IsArray(thresholdModeArray)) {
         DISPLAY_HILOGW(FEAT_BRIGHTNESS, "root <%{public}s> is not Array!", CONFIG_NAME.c_str());
+        cJSON_Delete(const_cast<cJSON*>(root));
         return false;
     }
-    for (auto value : root["thresholdMode"]) {
-        LuxThresholdConfig::Mode config{};
-        if (!value["modeName"].isString() || value["modeName"].asString().empty()) {
-            DISPLAY_HILOGW(FEAT_BRIGHTNESS, "<%{public}s> modeName is not find!", CONFIG_NAME.c_str());
+
+    cJSON* item = nullptr;
+    cJSON_ArrayForEach(item, thresholdModeArray) {
+        if (!cJSON_IsObject(item)) {
             continue;
         }
-        std::string name = value["modeName"].asString();
-        if (value["brightenDebounceTime"].isInt()) {
-            config.brightenDebounceTime = value["brightenDebounceTime"].asInt();
-        }
-        if (value["darkenDebounceTime"].isInt()) {
-            config.darkenDebounceTime = value["darkenDebounceTime"].asInt();
-        }
-        ConfigParserBase::Get().ParsePointXy(value, "brightenPoints", config.brightenPoints);
-        ConfigParserBase::Get().ParsePointXy(value, "darkenPoints", config.darkenPoints);
-        data.modeArray.insert(std::make_pair(name, config));
-        DISPLAY_HILOGI(FEAT_BRIGHTNESS, "<%{public}s> is insert!", name.c_str());
+        LuxThresholdParseConfigParams(item, data);
     }
+
+    cJSON_Delete(const_cast<cJSON*>(root));
     DISPLAY_HILOGI(FEAT_BRIGHTNESS, "[%{public}d] parse LuxThresholdConfigParser over!", displayId);
     return true;
 }
